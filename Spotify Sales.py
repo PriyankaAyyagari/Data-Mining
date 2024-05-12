9e50a345bb304aab73ff9daaeb2d21ccc270c25a
Priyanka Ayyagari <143545768+PriyankaAyyagari@users.noreply.github.com>
2024-05-12 15:57:27 -0700

    Add files via upload

--- Spotify Sales.py
+++ Spotify Sales.py
@@ -13,7 +13,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load the dataset
-file_path = "C:\\Users\\nidhi\\Downloads\\Data1.csv"
+file_path = "C:\\Users\\pinku\\Downloads\\Data1.csv"
 df = pd.read_csv(file_path)
 
 # Display the first few rows of the initial dataset
